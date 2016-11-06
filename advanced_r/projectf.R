22b512009927902a852e8af1723b71d70f648bbd
Mohit <shah1mohit@gmail.com>
2016-11-06 15:40:00 -0700

    Added functions

--- advanced_r/projectf.R
+++ advanced_r/projectf.R
@@ -1,2 +1,9 @@
 # read data file 
-read.csv("data/gapminder.csv", header = TRUE)
+
+CreatePlot <- function(cnty){
+  data <- read.csv("data/gapminder.csv", header = TRUE)
+  subData <- subset(data, data$country = cnty, header = TRUE )
+  plot(subData[2],subData[6],xlab = "Year", ylab = "GDP")
+}
+
+CreatePlot('Algeria')
