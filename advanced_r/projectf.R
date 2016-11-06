47eae732df2ef9c72964acc9938cf0df706b7e4d
Olshansky <Olshansky>
2016-11-06 16:45:25 -0700

    finsh up project

--- advanced_r/projectf.R
+++ advanced_r/projectf.R
@@ -1,8 +1,7 @@
-# read data file 
 
 CreatePlot <- function(cnty){
-  data <- read.csv("data/gapminder.csv", header = TRUE)
-  subData <- subset(data, data$country == cnty, header = TRUE )
+  dat <- read.csv("data/gapminder.csv", header = TRUE)
+  subData <- subset(dat, dat$country == cnty, header = TRUE )
   plot(subData$year,subData$gdpPercap,xlab = "Year", ylab = "GDP", main = cnty)
 }
 
@@ -15,3 +14,17 @@ for(x in Cuntry_input){
   CreatePlot(x)
 }
 
+
+
+dat <- read.csv("data/gapminder.csv", header = TRUE)
+conlist <- c('Asia' ,  'Africa' , 'Americas')
+for (x in conlist){
+datCon <- subset(dat,dat$continent == x)
+print(x)
+print(mean(datCon$lifeExp))
+print(min(datCon$lifeExp))
+print(max(datCon$lifeExp))
+}
+
+hist(dat$lifeExp)
+hist(dat$lifeExp,breaks = 100)
